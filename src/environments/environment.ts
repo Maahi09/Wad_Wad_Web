65b13b9e427306b18f8aa4143c254d92ccf6fa3a
Maahi <maahi.igeek@gmail.com>
2023-10-11 17:19:42 +0530

    changes in ui of pet-details

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    BASE_URL:'https://d51b-2405-201-2014-31ae-58e9-99f7-f1d7-1691.ngrok-free.app/'
+    BASE_URL:'https://c627-2405-201-2014-31ae-8115-6d30-fbdc-e369.ngrok-free.app/'
     // BASE_URL:'http://3.88.137.3/'
     
 }
